361924c8b42bb57d80a4a83e8c057cf639e85f6d
ehsan <eysoon2@gmail.com>
2017-01-13 09:58:00 +0330

    initial commit

---
+++ app/src/main/java/ehsan_hn/github/com/mafia/MainActivity.java
@@ -0,0 +1,50 @@
+package ehsan_hn.github.com.mafia;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager=getSupportFragmentManager();
+
+        fragment = new GameFragment();
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.main_container, fragment).commit();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.learn_item:
+                        fragment = new LearnFragment();
+                        break;
+                    case R.id.character_item:
+                        fragment = new CharacterFragment();
+                        break;
+                    case R.id.game_item:
+                        fragment = new GameFragment();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container, fragment).commit();
+                return true;
+            }
+        });
+    }
+}
